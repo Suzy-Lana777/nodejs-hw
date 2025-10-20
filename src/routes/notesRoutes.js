75adf4bff5bc85d0bdbe57c1f718f97d71ae9b00
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-10-20 21:19:24 +0300

    valid

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,6 +1,8 @@
 // src/routes/notesRoutes.js
 
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
+
 import {
   getAllNotes,
   getNoteById,
@@ -9,12 +11,28 @@ import {
   deleteNote,
 } from '../controllers/notesController.js';
 
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
+
 const router = Router();
 
-router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteById);
-router.post('/notes', createNote);
-router.patch('/notes/:noteId', updateNote);
-router.delete('/notes/:noteId', deleteNote);
+// GET /notes?page=&perPage=&tag=&search=
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+
+// GET /notes/:noteId
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+
+// POST /notes
+router.post('/notes', celebrate(createNoteSchema), createNote);
+
+// PATCH /notes/:noteId
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
+
+// DELETE /notes/:noteId
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 
 export default router;
