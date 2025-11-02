15e8f5b92b19d8bb629ec123ef8212c3d1441f2d
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-11-02 14:36:06 +0200

    problem with domen

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,14 +1,27 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+import { Resend } from 'resend';
 
-export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendMail = async (options) => {
+  return resend.emails.send({
+    from: process.env.RESEND_FROM,
+    to: options.to,
+    subject: options.subject,
+    html: options.html,
+  });
 };
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: process.env.SMTP_PORT,
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASSWORD,
+//   },
+// });
+
+// export const sendEmail = async (options) => {
+//   return await transporter.sendMail(options);
+// };
